742a571e6e884798c368781d42f50cbd865a61ac
Ryan Alyn Porter <rap@endymion.com>
2014-05-10 20:13:28 -0400

    Added a controller for creating URLs.

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,24 @@
+class UrlsController < ApplicationController
+  before_action :set_url, only: [:show, :edit, :update, :destroy]
+
+  # GET /urls/new
+  def new
+  end
+
+  # POST /urls
+  # POST /urls.json
+  def create
+    @url = Url.new(url_params)
+
+    respond_to do |format|
+      if @url.save
+        format.html { redirect_to @url, notice: 'Url was successfully created.' }
+        format.json { render :show, status: :created, location: @url }
+      else
+        format.html { render :new }
+        format.json { render json: @url.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  resources :urls
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   get '/:id' => "shortener/shortened_urls#show"
 
   # You can have the root of your site routed with "root"
-  # root 'urls#create'
+  root 'urls#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
