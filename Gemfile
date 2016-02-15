57ce6f3bb7f7e3937560074499a46fa343b8b0f8
Vivek <vivekpatna52@gmail.com>
2016-02-15 18:42:53 +0530

    multiple domain url creation logic added

--- Gemfile
+++ Gemfile
@@ -61,4 +61,5 @@ end
 group :development, :test do
   gem 'byebug'
   gem 'rspec-rails', '~> 3.4.2'
+  gem "better_errors"
 end
--- features/step_definitions/url_steps.rb
+++ features/step_definitions/url_steps.rb
@@ -14,10 +14,24 @@ Then(/^I should have (\d+) Shorted URL$/) do |count|
   expect(Shortener::ShortenedUrl.count).to eq count.to_i
 end
 
+Then(/^I should have Shorted URL with "(.*)\/(.*)" as url and "([^\"]*)" as target$/) do |domain, unique_key, url|
+  expect(
+    Shortener::ShortenedUrl.find_by(
+      domain_name: domain,
+      unique_key: unique_key,
+      url: url
+    )
+  ).to_not be nil
+end
+
 Then(/^I should have (\d+) Shorted URL with domain name as "([^"]*)"$/) do |count, domain_name|
   expect(Shortener::ShortenedUrl.where(domain_name: domain_name).count).to eq count.to_i
 end
 
 Then /^I should see "([^\"]*)" domain checkbox as checked$/ do |domain|
   expect(response).to have_selector("input", id: "domain_name_input_#{domain}", checked: "checked")
+end
+
+Then /^I should see "([^\"]*)" domain checkbox as unchecked$/ do |domain|
+  expect(response).to_not have_selector("input", id: "domain_name_input_#{domain}", checked: "checked")
 end
